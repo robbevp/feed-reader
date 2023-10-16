25f2f82d1a976a286b2a3022cfaca288700d7f1b
Robbe Van Petegem <git@robbevp.be>
2023-10-16 21:25:50 +0200

    Redirect user to sign in if not logged in (#69)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,23 @@ class ApplicationController < ActionController::Base
 
   default_form_builder ::ComponentFormBuilder
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   helper_method :current_user
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  private
+
+  def user_not_authorized
+    if current_user.present?
+      flash[:danger] = t 'application_controller.helpers.user_not_authorized.forbidden'
+      redirect_to request.referer || root_url
+    else
+      flash[:danger] = t 'application_controller.helpers.user_not_authorized.sign_in'
+      redirect_to new_session_path
+    end
   end
 end

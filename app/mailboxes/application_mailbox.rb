6c8fadac2184ecc13d64ffa7be2ab2aa85681457
Robbe Van Petegem <git@robbevp.be>
2023-10-27 21:21:39 +0200

    Create newsletter mailbox

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationMailbox < ActionMailbox::Base
+  routing(/\Anewsletter-\d+\+\h+/i => :newsletter)
 end
---
+++ app/mailboxes/newsletter_mailbox.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class NewsletterMailbox < ApplicationMailbox
+  MAIL_KEYS = %w[to from multipart date subject message_id].freeze
+
+  before_processing :set_newsletter
+
+  def process
+    data = MAIL_KEYS.index_with { |key| mail[key].to_s }
+    data['body'] = mail_body
+
+    Entry.create!(subscription: @newsletter.subscription, title: data['subject'], author: data['from'],
+                  published_at: data['date'], body: data['body'], external_id: data['message_id'], data:)
+  end
+
+  private
+
+  def set_newsletter
+    match_data = mail.to.first.match(/newsletter-(\d+)\+(\h+)@/)
+    @newsletter = Newsletter.find_by(id: match_data[1], public_id: match_data[2])
+    bounced! unless @newsletter.present?
+  end
+
+  def mail_body
+    if mail.html_part
+      mail.html_part.decoded
+    elsif mail.text_part
+      mail.text_part.decoded
+    else
+      mail.decoded
+    end
+  end
+end

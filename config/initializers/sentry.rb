7594695e7d764be044b1356aaa362b56e7ca0616
Robbe Van Petegem <git@robbevp.be>
2023-11-01 22:45:55 +0100

    Allow sentry sample rate to be configured

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,7 +6,6 @@ if dsn.present?
   Sentry.init do |config|
     config.dsn = dsn
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
-    # Disable performance monitoring
-    config.traces_sample_rate = 0
+    config.traces_sample_rate = ENV.fetch('SENTRY_TRACES_SAMPLE_RATE', 0)
   end
 end

a8f4b776f2dd4a362f5009e11c7181e86476bdb2
Robbe Van Petegem <git@robbevp.be>
2023-11-01 22:58:14 +0100

    Make sure traces are correct type (#100)

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,6 +6,6 @@ if dsn.present?
   Sentry.init do |config|
     config.dsn = dsn
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
-    config.traces_sample_rate = ENV.fetch('SENTRY_TRACES_SAMPLE_RATE', 0)
+    config.traces_sample_rate = ENV.fetch('SENTRY_TRACES_SAMPLE_RATE', 0).to_f
   end
 end

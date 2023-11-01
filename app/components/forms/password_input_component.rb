f381e8bc7f11f5d9223cd039a69cd34498bc35bd
Robbe Van Petegem <git@robbevp.be>
2023-11-01 22:07:22 +0100

    Render category text options with datalist

--- app/components/forms/password_input_component.rb
+++ app/components/forms/password_input_component.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Forms::PasswordInputComponent < Forms::BaseComponent
+  def initialize(**)
+    super(**)
+    raise 'Options is not supported for password inputs' if options.present?
+  end
 end
--- app/components/forms/radio_input_component.rb
+++ app/components/forms/radio_input_component.rb
@@ -1,10 +1,4 @@
 # frozen_string_literal: true
 
 class Forms::RadioInputComponent < Forms::BaseComponent
-  attr_reader :collection
-
-  def initialize(**args)
-    @collection = args.delete(:collection)
-    super(**args)
-  end
 end

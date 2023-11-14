8b0134759f7e704d77912910d64150f32fcf3fbc
Robbe Van Petegem <git@robbevp.be>
2023-11-14 20:15:13 +0100

    Handle `Errno::EBUSY` errors when fetching resources (#126)

--- app/jobs/fetch_proxied_image_job.rb
+++ app/jobs/fetch_proxied_image_job.rb
@@ -3,6 +3,9 @@
 class FetchProxiedImageJob < ApplicationJob
   queue_as :default
 
+  # Retry when we can't open a TCP connection
+  retry_on Errno::EBUSY, wait: :polynomially_longer, attempts: 10
+
   def perform(proxied_image)
     proxied_image.process
   end

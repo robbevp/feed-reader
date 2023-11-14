1a884082f17a905af0a7c7c963d2059d4b3fb045
Robbe Van Petegem <git@robbevp.be>
2023-11-14 20:12:50 +0100

    Handle `Errno::EBUSY` errors when fetching resources

--- app/jobs/refresh_rss_feed_job.rb
+++ app/jobs/refresh_rss_feed_job.rb
@@ -3,6 +3,9 @@
 class RefreshRssFeedJob < ApplicationJob
   queue_as :default
 
+  # Ignore when we can't open a connection - we'll just refresh later on
+  discard_on Errno::EBUSY
+
   def perform(rss_feed)
     rss_feed.refresh!
   end

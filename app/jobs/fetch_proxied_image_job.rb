a856968fcb80998e4bb944742d951483af7c204f
Robbe Van Petegem <git@robbevp.be>
2024-02-15 20:15:15 +0100

    Retry fetching image when we can't open a connection (#226)

--- app/jobs/fetch_proxied_image_job.rb
+++ app/jobs/fetch_proxied_image_job.rb
@@ -3,8 +3,8 @@
 class FetchProxiedImageJob < ApplicationJob
   queue_as :default
 
-  # Retry when we can't open a TCP connection
-  retry_on Errno::EBUSY, wait: :polynomially_longer, attempts: 10
+  # Retry when we can't open a connection or get a server error
+  retry_on Errno::EBUSY, Net::HTTPFatalError, Socket::ResolutionError, wait: :polynomially_longer, attempts: 10
 
   # Ignore any resource that return a 404 error
   discard_on ImageNotFoundError

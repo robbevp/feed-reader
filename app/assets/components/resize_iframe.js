7f4af100035c3e4b676e392e9dacbcec6bf70ce2
Robbe Van Petegem <git@robbevp.be>
2024-03-10 13:46:43 +0100

    Improve resizing of iframe

--- app/assets/components/resize_iframe.js
+++ app/assets/components/resize_iframe.js
@@ -2,21 +2,23 @@ import { SprinklesComponent } from "../lib/sprinkles";
 
 export default class ResizeIframeComponent extends SprinklesComponent {
   connect() {
-    document.addEventListener("load", () => this.setCorrectHeight());
-    this.element.contentWindow.addEventListener("load", () =>
-      this.setCorrectHeight(),
+    this.element.contentWindow.addEventListener(
+      "load",
+      this.setCorrectHeight.bind(this),
     );
-    window.addEventListener("resize", () => this.setCorrectHeight());
-    this.setCorrectHeight();
+    window.addEventListener("resize", this.setCorrectHeight.bind(this));
+
+    // Already set height in next frame (when text is loaded, but resources might not be)
+    window.requestAnimationFrame(this.setCorrectHeight.bind(this));
   }
 
   disconnect() {
-    document.removeEventListener("load", () => this.setCorrectHeight());
-    window.removeEventListener("resize", () => this.setCorrectHeight());
+    document.removeEventListener("load", this.setCorrectHeight.bind(this));
+    window.removeEventListener("resize", this.setCorrectHeight.bind(this));
   }
 
   setCorrectHeight() {
-    const height = this.element.contentDocument.body.scrollHeight;
+    const height = this.element.contentDocument.body?.scrollHeight || 500;
     // Add extra padding, since the body margin is not taken into account
     this.element.height = `${height + 40}px`;
   }

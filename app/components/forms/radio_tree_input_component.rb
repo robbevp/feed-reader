380576aee330a585058dde0cfa77057b28a567f4
Robbe Van Petegem <git@robbevp.be>
2025-01-27 19:37:07 +0100

    Add an option to show all categories (#606)

--- app/components/forms/radio_tree_input_component.rb
+++ app/components/forms/radio_tree_input_component.rb
@@ -3,11 +3,22 @@
 class Forms::RadioTreeInputComponent < Forms::BaseComponent
   include Forms::HasOptions
 
+  attr_reader :show_all
+
+  def initialize(show_all: nil, **)
+    super(**)
+    @show_all = show_all
+  end
+
   def roots
     @roots ||= options.filter { |opt| opt.parent_id.nil? }
   end
 
   def children_for(option)
     options.filter { |opt| opt.parent_id == option.id }
+  end
+
+  def show_all?
+    @show_all.present?
   end
 end

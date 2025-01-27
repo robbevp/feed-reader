ab36f702a09f4983f2a3fe90d2a914c23ef134a3
Robbe Van Petegem <git@robbevp.be>
2025-01-27 19:34:58 +0100

    Add an option to show all categories

--- app/components/concerns/forms/has_options.rb
+++ app/components/concerns/forms/has_options.rb
@@ -3,14 +3,15 @@
 module Forms::HasOptions
   extend ActiveSupport::Concern
 
+  # We need to define `initialize` outside of included, so that we can still add another init in the actual class
+  def initialize(options: [], value_method: :first, label_method: :second, **)
+    super(**)
+    @options = options.map { |opt| opt.is_a?(ApplicationRecord) ? opt : Array.wrap(opt) }
+    @label_method = label_method
+    @value_method = value_method
+  end
+
   included do
     attr_reader :options, :value_method, :label_method
-
-    def initialize(options: [], value_method: :first, label_method: :second, **)
-      super(**)
-      @options = options.map { |opt| opt.is_a?(ApplicationRecord) ? opt : Array.wrap(opt) }
-      @label_method = label_method
-      @value_method = value_method
-    end
   end
 end

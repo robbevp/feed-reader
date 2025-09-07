6a4dae5ebf2794821704d839f50605431aa07cd4
Robbe Van Petegem <git@robbevp.be>
2025-09-07 11:14:22 +0200

    Mount health endpoint on `/up`

--- config/routes.rb
+++ config/routes.rb
@@ -85,6 +85,10 @@
 #     frontend_static GET    /frontend/static/:version/:id(.:format)   good_job/frontends#static {:version=>"4-6-0"}
 
 Rails.application.routes.draw do
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

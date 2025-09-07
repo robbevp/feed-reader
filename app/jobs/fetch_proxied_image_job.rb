2dd492c1bdfd878f22c5b3cde6bd2340dce66492
Robbe Van Petegem <git@robbevp.be>
2025-09-07 11:29:56 +0200

    Capture more errors when fetching feeds and images

--- app/jobs/fetch_proxied_image_job.rb
+++ app/jobs/fetch_proxied_image_job.rb
@@ -4,7 +4,9 @@ class FetchProxiedImageJob < ApplicationJob
   queue_as :default
 
   # Retry when we can't open a connection or get a server error
-  retry_on Errno::EBUSY, Net::HTTPFatalError, Socket::ResolutionError, wait: :polynomially_longer, attempts: 10
+  retry_on Errno::EBUSY, Net::HTTPFatalError, Socket::ResolutionError, Net::OpenTimeout, Net::ReadTimeout,
+           OpenSSL::SSL::SSLError,
+           wait: :polynomially_longer, attempts: 10
 
   # Ignore any client errors, as we can't fix these issues
   discard_on Net::HTTPClientException

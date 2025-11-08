07ab11cc68b2746b131487d4a5389aeced193d79
Robbe Van Petegem <git@robbevp.be>
2025-11-08 10:04:24 +0100

    Upgrade pagy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include Pagy::Backend
+  include Pagy::Method
   include Pundit::Authorization
 
   default_form_builder ::ComponentFormBuilder

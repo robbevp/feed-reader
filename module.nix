10e6af0a602313341b9a17b84f3c0edf2f50a2f0
Robbe Van Petegem <git@robbevp.be>
2026-01-06 19:39:12 +0100

    Fix date package

--- module.nix
+++ module.nix
@@ -59,7 +59,7 @@ let
     ${exports}
     export $(${pkgs.coreutils}/bin/cat ${cfg.environmentFile} | ${pkgs.findutils}/bin/xargs)
     cd ${feed-reader}
-    FILENAME=/var/log/feed_reader/postfix-`date +"%Y-%m-%d %T.%N"`.log
+    FILENAME=/var/log/feed_reader/postfix-`${pkgs.coreutils}/bin/date +"%Y-%m-%d %T.%N"`.log
     ${feed-reader.env}/bin/bundle exec rails action_mailbox:ingress:postfix URL='https://${cfg.hostname}/rails/action_mailbox/relay/inbound_emails' INGRESS_PASSWORD=$RAILS_INBOUND_EMAIL_PASSWORD >$FILENAME 2>&1
     ${pkgs.coreutils}/bin/tail -n 1 $FILENAME
   '';

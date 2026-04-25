f697c67afb698e9cda6bacab40b81819094b02fe
Robbe Van Petegem <git@robbevp.be>
2026-04-25 13:55:53 +0200

    Switch rails to 8.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,11 +50,6 @@ Rails.application.configure do
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "feed_reader_production"
-
-  # Disable caching for Action Mailer templates even if Action Controller
-  # caching is enabled.
-  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

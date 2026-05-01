de5eba42d36d69fb4a64fb0b8a8902434adef10c
Robbe Van Petegem <git@robbevp.be>
2026-05-01 10:42:46 +0200

    Fix typo

--- module.nix
+++ module.nix
@@ -68,7 +68,7 @@ let
     runtimeEnv = env;
 
     text = ''
-      export cat ${cfg.environmentFile} | xargs)
+      export $(cat ${cfg.environmentFile} | xargs)
       cd ${feed-reader}
       FILENAME=/var/log/feed_reader/postfix-debug.log
       ${feed-reader.env}/bin/bundle exec rails action_mailbox:ingress:postfix URL='https://${cfg.hostname}/rails/action_mailbox/relay/inbound_emails' INGRESS_PASSWORD=$RAILS_INBOUND_EMAIL_PASSWORD >$FILENAME 2>&1

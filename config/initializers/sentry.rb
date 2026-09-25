d99a726ab51185012917b103dc20b94e5dbea4d3
Robbe Van Petegem <git@robbevp.be>
2026-09-25 11:41:59 +0200

    Don't send logs to sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,6 +5,7 @@ dsn = ENV.fetch('SENTRY_DSN', nil)
 if dsn.present?
   Sentry.init do |config|
     config.dsn = dsn
+    config.rails.structured_logging.enabled = false
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
     config.traces_sample_rate = ENV.fetch('SENTRY_TRACES_SAMPLE_RATE', 0).to_f
   end
